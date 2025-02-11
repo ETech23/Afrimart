cd5841a6ded5bddf0bb92478f1e3de1690f59643
ETech23 <ogbaae@gmail.cm>
2025-02-11 07:35:22 +0100

    replica

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, w: "majority" });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
